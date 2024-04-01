d3443e230f3d24c9b11b79b903fadfbb6cbffaa8
phukon <hello@rikiphukon.com>
2024-04-02 05:26:09 +0530

    fix: Remove getPackageInfo function to fix crashes

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -2,19 +2,19 @@
 import { init } from "@/src/commands/init"
 import { Command } from "commander";
 
-import { getPackageInfo } from "@/src/utils/get-package-info";
+// import { getPackageInfo } from "@/src/utils/get-package-info";
 
 process.on("SIGINT", () => process.exit(0));
 process.on("SIGTERM", () => process.exit(0));
 
 async function main() {
-  const packageInfo = await getPackageInfo();
+  // const packageInfo = await getPackageInfo();
 
   const program = new Command()
     .name("setup-cli-app")
     .description("Set up a modern CLI app by running one command.")
     .version(
-      packageInfo.version || "1.0.0",
+      "1.0.3",
       "-v, --version",
       "display the version number"
     )
